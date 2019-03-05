885847f6eb02853ae65f656051288d334e722d39
Thibaut MELEN <thmelen@e2r8p7.42.fr>
2019-03-05 14:23:54 +0100

    mail

--- register.php
+++ register.php
@@ -63,72 +63,22 @@ if (isset($_POST['register_submit'])) {
                             $insertmbr = $bdd->prepare("INSERT INTO member(pseudo, email, pass, avatar, notif, mail_key, confirm) VALUES(?, ?, ?, ?, ?, ?, ?)");
                             $insertmbr->execute(array($register_pseudo, $register_email, $register_password, "data/avatar_member/default.jpg", 1, $key, 0));
 
-
-
-
-
-
-
-
-
-
-                            $mail = 'weaponsb@mail.fr'; // Déclaration de l'adresse de destination.
-                            if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
-                            {
-                                $passage_ligne = "\r\n";
-                            } else {
-                                $passage_ligne = "\n";
-                            }
-                                //=====Déclaration des messages au format texte et au format HTML.
-                            $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
-                            $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
-                                //==========
-                                
-
-                                //=====Création de la boundary.
-                            $boundary = "-----=" . md5(rand());
-                            $boundary_alt = "-----=" . md5(rand());
-                                //==========
-                                
-                                //=====Définition du sujet.
-                            $sujet = "Hey mon ami !";
-                                //=========
-                                
-                                //=====Création du header de l'e-mail.
-                            $header = "From: \"WeaponsB\"<weaponsb@mail.fr>" . $passage_ligne;
-                            $header .= "Reply-to: \"WeaponsB\" <weaponsb@mail.fr>" . $passage_ligne;
-                            $header .= "MIME-Version: 1.0" . $passage_ligne;
-                            $header .= "Content-Type: multipart/mixed;" . $passage_ligne . " boundary=\"$boundary\"" . $passage_ligne;
-                                //==========
-                                
-                                //=====Création du message.
-                            $message = $passage_ligne . "--" . $boundary . $passage_ligne;
-                            $message .= "Content-Type: multipart/alternative;" . $passage_ligne . " boundary=\"$boundary_alt\"" . $passage_ligne;
-                            $message .= $passage_ligne . "--" . $boundary_alt . $passage_ligne;
-                                //=====Ajout du message au format texte.
-                            $message .= "Content-Type: text/plain; charset=\"ISO-8859-1\"" . $passage_ligne;
-                            $message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
-                            $message .= $passage_ligne . $message_txt . $passage_ligne;
-                                //==========
-
-                            $message .= $passage_ligne . "--" . $boundary_alt . $passage_ligne;
-                                
-                                //=====Ajout du message au format HTML.
-                            $message .= "Content-Type: text/html; charset=\"ISO-8859-1\"" . $passage_ligne;
-                            $message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
-                            $message .= $passage_ligne . $message_html . $passage_ligne;
-                                //==========
-                                
-                                //=====On ferme la boundary alternative.
-                            $message .= $passage_ligne . "--" . $boundary_alt . "--" . $passage_ligne;
-                                //==========
-
-                           $message .= $passage_ligne . "--" . $boundary . $passage_ligne;
-                          
-  
+                            // Préparation du mail contenant le lien d'activation
+                            $sujet = "Activer votre compte" ;
+                            $header = "From: no-reply@camagru.com" ;
                             
-                            //=====Envoi de l'e-mail.
-    
+                            // Le lien d'activation est composé du login(log) et de la clé(cle)
+                            $message = 'Bienvenue sur Camagru,
+                            
+                            Pour activer votre compte, veuillez cliquer sur le lien ci dessous
+                            ou copier/coller dans votre navigateur internet.
+                            
+                            http://localhost:8080/login.php?log='.urlencode($login).'&cle='.urlencode($cle).'
+                            
+                            
+                            ---------------
+                            Ceci est un mail automatique, Merci de ne pas y répondre.';
+                                
                             if (mail($register_email, $sujet, $message, $header))
                                 echo "Work work";
                             else
